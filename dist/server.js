505bddda942dfd1ff75e8c95f7d56990dae4a240
Ricardo Bohadana Martins <cacolorde@gmail.com>
2022-03-10 00:27:18 -0300

    update cors headers

--- dist/server.js
+++ dist/server.js
@@ -20,6 +20,16 @@ const app = (0, express_1.default)();
 exports.authTimeout = "10m";
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "https://jwt-auth-login-page.vercel.app");
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    // Request headers you wish to allow
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    // Pass to next layer of middleware
+    next();
+});
 app.use((0, cors_1.default)({
     origin: "https://jwt-auth-login-page.vercel.app",
 }));
--- server.ts
+++ server.ts
@@ -16,6 +16,28 @@ export const authTimeout = "10m";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://jwt-auth-login-page.vercel.app"
+    );
+
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(
     cors({
         origin: "https://jwt-auth-login-page.vercel.app",
